1a7fb69c0998efbb7ad8875a65c577b1bc881963
Владислав Баженов <v.v.bazhenov@yandex.ru>
2023-12-03 23:17:59 +0600

    Пример использования цикла for

---
+++ CycleFor/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Пример использование цикла for C#
+ * Условия задачи: вывести на экран все чётные числа от 1 до N включительно
+ * Код опубликован на странице сайта https://csharp.webdelphi.ru/cikly-c/
+ */
+namespace CycleFor
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int N = 10; //задаем число до которого будем проверять все числа на чётность
+			for (int i = 1; i <= N; i++)
+			{
+				if (i % 2 == 0)
+					Console.WriteLine($"Число {i} - чётное");
+			}
+		}
+	}
+}
