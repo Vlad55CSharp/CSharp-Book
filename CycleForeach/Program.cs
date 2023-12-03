369bd87edb8aa3e0272d5b7620830c4ea79d8608
Владислав Баженов <v.v.bazhenov@yandex.ru>
2023-12-04 00:25:08 +0600

    Пример использование цикла foreach

---
+++ CycleForeach/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Пример использование цикла foreach C#
+ * Условия задачи: вывести каждый символ введенной пользователем строки 
+ * Код опубликован на странице сайта https://csharp.webdelphi.ru/cikly-c/
+ */
+namespace CycleForeach
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите строку: ");
+             string str = Console.ReadLine();
+			foreach (char ch in str) 
+			{
+				Console.WriteLine(ch);
+			}
+		}
+	}
+}
