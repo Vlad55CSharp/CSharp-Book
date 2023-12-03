b96cf598cbbdd7db8892561d3dfb976e78ada2d6
Владислав Баженов <v.v.bazhenov@yandex.ru>
2023-12-04 00:17:08 +0600

    Пример использование цикла do..while

---
+++ CycleDoWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Пример использование цикла do..while C#
+ * Условия задачи: пользователь вводит с клавиатуры целые числа. До тех пор, пока пользователь не введет 0 определять минимальное из введенных значений
+ * Код опубликован на странице сайта https://csharp.webdelphi.ru/cikly-c/
+ */
+namespace CycleDoWhile
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            int min = int.MaxValue;
+			int num;
+			do
+			{
+				Console.Write("Введите целое число: ");
+				num = Convert.ToInt32(Console.ReadLine());
+				if ((num<min) && (num != 0))
+				{
+				   min = num;
+				}
+			}
+			while (num != 0);
+			Console.WriteLine($"Минимальное значение {min}");
+		}
+	}
+}
