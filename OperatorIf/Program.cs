4b93d3f15447afb9a87c750ed5188d86c42778ec
Владислав Баженов <v.v.bazhenov@yandex.ru>
2023-12-03 19:18:02 +0600

    Пример использования условного оператора if

---
+++ OperatorIf/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Пример использование условного оператора if в C#
+ * Условия задачи: заданы два числа a и b, если a меньше b, то необходимо вывести на экран их сумму, иначе - вывести на экран их разность
+ * Код опубликован на странице сайта https://csharp.webdelphi.ru/logicheskie-operatory-c/#_if
+ */
+namespace OperatorIf
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = 3; //задаем значение переменной a
+			int b = 4; //задаем значение переменной b
+			if (a < b) //проверяем условие: a меньше, чем b
+			{
+				Console.WriteLine(a + b); //если условие соблюдается 
+			}
+			else 
+			{
+				Console.WriteLine(a - b); //если условие НЕ соблюдается 
+			}
+		}
+	}
+}
