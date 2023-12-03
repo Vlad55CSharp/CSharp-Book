eeddb89f95076f0e8f67c97b003e8d0c56b30287
Владислав Баженов <v.v.bazhenov@yandex.ru>
2023-12-04 04:55:12 +0600

    Пример работы с результатами методов

---
+++ ReturnValues/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ReturnValues
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int value = Sum(10, 4); //присвоили переменной значение, возвращаемое методом
+			Console.WriteLine(value); //вывели значение в консоль	
+			Console.WriteLine(Sum(5,5)); //рассчитали сумму и сразу вывели в консоль вызвав метод Console.WriteLine()
+			
+			CheckAge(15);
+			Console.WriteLine("Вышли из метода CheckAge");
+		}
+
+		static int Sum(int a, int b) => a + b;
+
+		static void CheckAge(int age)
+		{
+			if (age < 18) 
+			{
+				Console.WriteLine("Этот контент имеет ограничение 18+");
+				return; 
+			}
+			Console.WriteLine("Вы достигли необходимого возраста - наслаждайтесь");
+		}
+
+	}
+}
