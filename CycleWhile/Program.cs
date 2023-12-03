46a4b154dba6988527e2e6dbde82f569e96f0c1c
Владислав Баженов <v.v.bazhenov@yandex.ru>
2023-12-03 23:44:05 +0600

    Пример использование цикла while

---
+++ CycleWhile/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Пример использование цикла while C#
+ * Условия задачи: выводить на экран четные числа, начиная с 2 до тех пор пока их сумма не станет больше 200
+ * Код опубликован на странице сайта https://csharp.webdelphi.ru/cikly-c/
+ */
+namespace CycleWhile
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int i = 2;
+			int sum = 0;
+			while (sum < 200)
+			{
+				sum += i;
+				Console.WriteLine($"Число {i} чётное");
+				i += 2;
+				Console.WriteLine($"Сумма чётных чисел {sum}");
+			}
+		}
+	}
+}
